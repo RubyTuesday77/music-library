839f8e7534428e02827835dbc14425c324e2157f
Tyson Lannon <105758468+RubyTuesday77@users.noreply.github.com>
2022-10-26 20:28:29 -0400

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,9 @@ function SearchBar(props) {
     // let [searchTerm, setSearchTerm] = useState('')
     
     return (
-        <form>
+        <form onSubmit={ (e) => props.handleSearch(e, searchTerm) }>
             <input type="text" placeholder="Search Here"
-                onChange={ (e) => props.handleSearch(e, e.target.value) } />
+                onChange={ (e) => setSearchTerm(e.target.value) } />
             <input type="submit" />
         </form>
     )
