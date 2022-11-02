d88db654ec09fb0c11495bb6f14788deb0b7bc0d
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-11-02 14:46:46 -0400

    get main branch updated

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
 import GalleryItem from './GalleryItem'
 
 function Gallery(props) {
+  
   const display = props.data.map((item, index) => {
     return (
       <GalleryItem item={ item } key={ index } />
