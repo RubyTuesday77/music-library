458523a3a409234829b0ce4323e110c03cc676bd
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-11-02 19:03:52 -0400

    before withContext merge

--- src/App.js
+++ src/App.js
@@ -7,51 +7,51 @@ import ArtistView from './components/ArtistView'
 import AlbumView from './components/AlbumView'
 
 function App() {
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
 
-    const API_URL = 'https://itunes.apple.com/search?term='
+  const API_URL = 'https://itunes.apple.com/search?term='
 
-    useEffect(() => {
-      if(search) {
-        const fetchData = async () => {
-          document.title = `${search} Music`
-          const response = await fetch(API_URL + search)
-          const resData = await response.json()
-          console.log(resData)
-          if(resData.results.length > 0) {
-            setData(resData.results)
-          } else {
-            setMessage('Not Found')
-          }
+  useEffect(() => {
+    if(search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        console.log(resData)
+        if(resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
         }
-        fetchData()
       }
-    }, [search])
-
-    const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
+      fetchData()
     }
+  }, [search])
 
-    return (
-        <div className="App">
-          {message}
-          <Router>
-            <Routes>
-              <Route path="/" element={
-                <>
-                  <SearchBar handleSearch={ handleSearch } />
-                  <Gallery data={ data } />
-                </>
-              } />
-              <Route path="/album/:id" element={ <AlbumView /> } />
-              <Route path="/artist/:id" element={ <ArtistView /> } />
-            </Routes>
-          </Router>
-        </div>
-    )
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
+  return (
+    <div className="App">
+    { message }
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <SearchBar handleSearch={ handleSearch } />
+            <Gallery data={ data } />
+          </>
+        } />
+        <Route path="/album/:id" element={ <AlbumView /> } />
+        <Route path="/artist/:id" element={ <ArtistView /> } />
+      </Routes>
+    </Router>
+  </div>
+);
 }
 
 export default App;
