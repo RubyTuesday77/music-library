739df6dcd98d8e8065ad815eca25a84a1ba29671
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-11-02 20:44:16 -0400

    fixed SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,8 @@ function SearchBar(props) {
     
     return (
         <form onSubmit={ (e) => props.handleSearch(e, searchTerm) }>
-            <input type="text" placeholder="Search Here" onChange={ (e) => setSearchTerm(e.target.value) } />
+            <input type="text" placeholder="Search Here"
+                onChange={ (e) => setSearchTerm(e.target.value) } />
             <input type="submit" />
         </form>
     )
