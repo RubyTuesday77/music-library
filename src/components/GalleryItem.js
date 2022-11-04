89bd70b1606b237cf1fd91c89c7c9d85348c363b
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-11-04 17:51:54 -0400

    merged branches

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function GalleryItem(props) {
     let [view, setView] = useState(false)
@@ -7,7 +8,8 @@ function GalleryItem(props) {
         'width': '25vw',
         'height': '20vh',
         'border': '1px solid black',
-        'margin': '2px'
+        'margin': '2px',
+        'position': 'relative'
     }
     
     const detailStyle = {
@@ -15,6 +17,7 @@ function GalleryItem(props) {
         'height': '20vh',
         'border': '1px solid black',
         'margin': '2px',
+        'position': 'relative',
         'backgroundImage': `url(${ props.item.artworkUrl100 })`,
         'backgroundRepeat': 'no-repeat',
         'backgroundSize': 'cover',
@@ -34,8 +37,17 @@ function GalleryItem(props) {
         return (
             <div style={ detailStyle }>
                 <h2>{ props.item.trackName }</h2>
-                <h3>{ props.item.collectionName }</h3>
-                <h4>{ props.item.primaryGenre }</h4>
+                <h3>
+                    <Link to={ `/artist/${props.item.artistId}` }>
+                        { props.item.artistName }
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={ `/album/${props.item.collectionId}` }>
+                        { props.item.collectionName }
+                    </Link>
+                </h3>
+                <h4>{ props.item.primaryGenreName }</h4>
                 <h4>{ props.item.releaseDate }</h4>
             </div>
         )
