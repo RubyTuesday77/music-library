fcb19f2162654c88f1e9212a416ae219f2504271
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-11-04 20:12:40 -0400

    made small updates

--- src/App.js
+++ src/App.js
@@ -9,47 +9,48 @@ import { DataContext } from './context/DataContext'
 import { SearchContext } from './context/SearchContext'
 
 function App() {
-    let searchInput = useRef('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+  let searchInput = useRef('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
 
-    const API_URL = 'https://itunes.apple.com/search?term='
+  const API_URL = 'https://itunes.apple.com/search?term='
 
-    const handleSearch = (e, term) => {
-      e.preventDefault()
-      const fetchData = async () => {
-        document.title = `${term} Music`
-        const response = await fetch(API_URL + term)
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          return setData(resData.results)
-        } else {
-          return setMessage('Not Found')
-        }
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    const fetchData = async () => {
+      document.title = `${term} Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if(resData.results.length > 0) {
+        return setData(resData.results)
+      } else {
+        return setMessage('Not Found')
       }
-      fetchData()
     }
+    fetchData()
+  }
 
-    return (
-        <div className="App">
-            <Router>
-            <Routes>
-              <Route path="/" element={
-                <>
-                  <SearchContext.Provider value={ { term: searchInput, handleSearch: handleSearch} }>
-                    <SearchBar />
-                  </SearchContext.Provider>
-                  <DataContext.Provider value={ data }>
-                    <Gallery />
-                  </DataContext.Provider>
-                </>
-              } />
-              <Route path="/album/:id" element={ <AlbumView /> } />
-              <Route path="/artist/:id" element={ <ArtistView /> } />
-            </Routes>
-          </Router>
-        </div>
-    )
+  return (
+    <div className="App">
+      {message}
+        <Router>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <SearchContext.Provider value={ { term: searchInput, handleSearch: handleSearch} }>
+                  <SearchBar />
+                </SearchContext.Provider>
+                <DataContext.Provider value={ data }>
+                  <Gallery />
+                </DataContext.Provider>
+              </>
+            } />
+            <Route path="/album/:id" element={ <AlbumView /> } />
+            <Route path="/artist/:id" element={ <ArtistView /> } />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
