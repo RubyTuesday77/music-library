8680be78b2c38f0a5122129e8afb92b30eae83b0
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-11-07 20:07:34 -0500

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
@@ -5,41 +6,35 @@ import { DataContext } from './context/DataContext'
 import { SearchContext } from './context/SearchContext'
 
 function App() {
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
-    let searchInput = useRef('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+  let searchInput = useRef('')
 
-    const API_URL = 'https://itunes.apple.com/search?term='
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    fetch(`https://itunes.apple.com/search?term=${term}`)
+        .then(response => response.json())
+        .then(resData => {
+          if(resData.results.length > 0) {
+            return setData(resData.results)
+          } else {
+            return setMessage('Not Found')
+          }
+        })
+      .catch(err => setMessage('An Error has Occurred!'))
+  }
 
-    const handleSearch = (e, term) => {
-      e.preventDefault()
-      const fetchData = async () => {
-        document.title = `${term} Music`
-        const response = await fetch(API_URL + term)
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          return setData(resData.results)
-        } else {
-          return setMessage('Not Found')
-        }
-      }
-      fetchData()
-    }
-
-    return (
-      <div className="App">
-        <SearchContext.Provider value={ {
-          term: searchInput,
-          handleSearch: handleSearch
-        } }>
-          <SearchBar />
-        </SearchContext.Provider>
-        {message}
-        <DataContext.Provider value={ data }>
-          <Gallery />
-        </DataContext.Provider>
-      </div>
-    )
+  return (
+    <div className="App">
+      <SearchContext.Provider value={{ term: searchInput, handleSearch: handleSearch }}>
+        <SearchBar />
+      </SearchContext.Provider>
+      { message }
+      <DataContext.Provider value={ data }>
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  )
 }
 
 export default App;
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -7,7 +7,8 @@ function GalleryItem(props) {
         'width': '25vw',
         'height': '20vh',
         'border': '1px solid black',
-        'margin': '2px'
+        'margin': '2px',
+        'position': 'relative'
     }
     
     const detailStyle = {
@@ -15,10 +16,11 @@ function GalleryItem(props) {
         'height': '20vh',
         'border': '1px solid black',
         'margin': '2px',
-        'backgroundImage': `url(${ props.item.artworkUrl100 })`,
+        'position': 'relative',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
         'backgroundRepeat': 'no-repeat',
         'backgroundSize': 'cover',
-        'color': 'yellow'
+        'color': 'white'
     }
 
     const simpleView = () => {
@@ -35,14 +37,14 @@ function GalleryItem(props) {
             <div style={ detailStyle }>
                 <h2>{ props.item.trackName }</h2>
                 <h3>{ props.item.collectionName }</h3>
-                <h4>{ props.item.primaryGenre }</h4>
+                <h4>{ props.item.primaryGenreName }</h4>
                 <h4>{ props.item.releaseDate }</h4>
             </div>
         )
     }
 
     return (
-        <div onClick={ () => setView(!view) } style={ { 'display': 'inline-block' } }>
+        <div onClick={ () => setView(!view) } style={{ 'display': 'inline-block' }}>
             { view ? detailView() : simpleView() }
         </div>
     )
